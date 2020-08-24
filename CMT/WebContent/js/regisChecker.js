364cc4b8fce18896ae104937125c2c802b244242
CristianCamillo <56133559+CristianCamillo@users.noreply.github.com>
2020-08-24 12:19:50 +0200

    cristian, fine 2 check forms

--- CMT/WebContent/js/regisChecker.js
+++ CMT/WebContent/js/regisChecker.js
@@ -5,5 +5,5 @@ function checkRegisForm()
 	const confPassword = document.forms["form"]["confPassword"];
 	const saldo = document.forms["form"]["saldo"];
 	
-	return username.style.color == "rgb(0, 128, 0)" && password.style.color == "rgb(0, 128, 0)" && password == confPassword && saldo.style.color == "rgb(0, 128, 0)"; 
+	return username.style.color == "rgb(0, 128, 0)" && password.style.color == "rgb(0, 128, 0)" && password.value == confPassword.value && saldo.style.color == "rgb(0, 128, 0)"; 
 }
